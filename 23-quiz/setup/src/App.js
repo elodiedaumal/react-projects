592f8032e9d74dc6e7318260dc32971cd88c0afb
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-02-01 15:14:36 +0100

    fetch my questions display the question in the web

--- 23-quiz/setup/src/App.js
+++ 23-quiz/setup/src/App.js
@@ -1,11 +1,57 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import { useGlobalContext } from './context';
 
-import SetupForm from './SetupForm'
-import Loading from './Loading'
-import Modal from './Modal'
+import SetupForm from './SetupForm';
+import Loading from './Loading';
+import Modal from './Modal';
 function App() {
-  return <h2>quiz starter</h2>
+  const {
+    homeWaiting,
+    loading,
+    questions,
+    questionIndex,
+    correctAnswer,
+    error,
+    isModalOpen,
+  } = useGlobalContext();
+  if (homeWaiting) {
+    return <SetupForm />;
+  }
+  if (loading) {
+    return <Loading />;
+  }
+  if (isModalOpen) {
+    return <Modal />;
+  }
+  const { incorrect_answers, correct_answer, question } = questions[0];
+  const answers = [...incorrect_answers, correct_answer];
+
+  return (
+    <main>
+      {/* <Modal /> */}
+      <section className='quiz'>
+        <p className='correct-answers'>
+          correct answers: {correctAnswer} / {questionIndex}
+        </p>
+        <article className='container'>
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+
+          <div className='btn-container'>
+            {answers.map((answers, index) => {
+              return (
+                <button
+                  className='answer-btn'
+                  key={index}
+                  dangerouslySetInnerHTML={{ __html: answers }}
+                  onClick={}
+                ></button>
+              );
+            })}
+          </div>
+        </article>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 23-quiz/setup/src/context.js
+++ 23-quiz/setup/src/context.js
@@ -1,24 +1,73 @@
-import axios from 'axios'
-import React, { useState, useContext, useEffect } from 'react'
+import axios from 'axios';
+import React, { useState, useContext, useEffect } from 'react';
 
 const table = {
   sports: 21,
   history: 23,
   politics: 24,
-}
+};
 
-const API_ENDPOINT = 'https://opentdb.com/api.php?'
+const API_ENDPOINT = 'https://opentdb.com/api.php?';
 
-const url = ''
+const url = '';
+const tempurl =
+  'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple';
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [homeWaiting, setHomeWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [correctAnswer, setCorrectAnswer] = useState(0);
+  const [error, setError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchQuestion = async (url) => {
+    setLoading(true);
+    setHomeWaiting(false);
+    const res = await axios(url).catch((err) => console.log(err));
+    if (res) {
+      if (res.data.results.length > 0) {
+        setQuestions(res.data.results);
+        setLoading(false);
+        setHomeWaiting(false);
+        setError(false);
+      } else {
+        setHomeWaiting(true);
+        setError(true);
+        setLoading(false);
+      }
+    } else {
+      setHomeWaiting(true);
+      setError(true);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestion(tempurl);
+  }, []);
+  return (
+    <AppContext.Provider
+      value={{
+        homeWaiting,
+        loading,
+        questions,
+        questionIndex,
+        correctAnswer,
+        error,
+        isModalOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
